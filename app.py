94fdaf398c150530cba2741c3c027965b84933cc
23098-code <23098@pua.ac.th>
2026-01-25 17:24:50 +0700

    Update app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Corn Disease Classification",
+    layout="centered"
+)
+
+st.title("🌽 Corn Disease Classification")
+st.write("เว็บนี้รันได้แล้ว 🎉")
+
+st.success("ถ้าเห็นหน้านี้ แปลว่า Streamlit ทำงานปกติ")
