effcd44bcd07dc11796efc4bad7d7081f12f2ffa
23098-code <23098@pua.ac.th>
2026-02-27 16:35:51 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const MODEL_PATH = location.href.includes("github.io")
-    ? "/the-classification-of-corn-diseases/my_model/"
-    : "./my_model/";
+const MODEL_PATH = location.hostname.includes("github.io")
+  ? "/the-classification-of-corn-diseases/my_model/"
+  : "./my_model/";
 
 let model;
 let webcam;
